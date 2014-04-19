eca8ae795e841e9958c5a73dc6f132bc5a1e9dd7
Brian Jackson <axnjaxn@axnjaxn.com>
2014-04-19 13:02:26 -0400

    Finished refactoring Move class.

--- src/GameState.java
+++ src/GameState.java
@@ -32,14 +32,17 @@ public class GameState {
 	}
 
 	public static class Move {
-		public Location location;
-		public Move next;
+		private Location location;
+		private Move next;
 
-		public Location from() {return location;}
-		public Location to() {return next.location;}
-		
 		public Move(Location location, Move next) {this.location = location; this.next = next;}
 		public Move(Location from, Location to) {this(from, new Move(to, (Move)null));}
+		
+		public Location from() {return location;}
+		public Location to() {return next.location;}
+		public boolean hasMore() {return next.next != null;}
+		public Move getNext() {return next;}
+		
 		public String toString() {return from() + " - " + to();}
 	}
 
